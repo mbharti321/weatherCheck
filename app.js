e2c0ab67e194c2f3f22116f8a77a5f4c4bddd6d5
mbharti321 <mbharti321@gmail.com>
2022-04-25 13:46:58 +0530

    fixed untracked files

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 app.get("/", function (req, res) {
@@ -11,33 +13,40 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-
-  // const queryCity = "Bengaluru";
   // reding city name entered by user
   var queryCity = req.body.cityName;
   queryCity = queryCity.toUpperCase();
-  const apiKey = "2d46b42460fc49e01757cecc18c83e78"//process.env.API_KEY;
   console.log(process.env.API_KEY);
+  const apiKey = "2d46b42460fc49e01757cecc18c83e78" //process.env.API_KEY;
   const unit = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity + "&units=" + unit + "&appid=" + apiKey + "";
 
+  // fetch data from opeanWeather API
   https.get(url, function (response) {
     // console.log(response);
     response.on("data", function (data) {
       // console.log(data);
       const weatherData = JSON.parse(data);
-      // res.send(weatherData);
-      // return;
+
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      
-      res.write("<h1> " + queryCity + "</h1>");
-      res.write("<h1>The weather is currently " + description + ". </h1>");
-      res.write("<h3>Current temperature is: " + temp + " degrees Celcius.</h3>");
-      res.write("<img src=" + imageURL + " alt=" + description + ">");
-      res.send();
+
+      // write data to webpage
+      // res.write("<h1> " + queryCity + "</h1>");
+      // res.write("<h1>The weather is currently " + description + ". </h1>");
+      // res.write("<h3>Current temperature is: " + temp + " degrees Celcius.</h3>");
+      // res.write("<img src=" + imageURL + " alt=" + description + ">");
+      // res.send();
+
+      const myWeatherData = {
+        queryCity: "queryCity",
+        description: "description",
+        temp: "temp",
+        description: "description"
+      }
+      res.render(__dirname + "/views/response.html", myWeatherData);
     });
   });
 });
@@ -52,4 +61,3 @@ app.listen(port, function () {
   console.log("Server Started successfully!! at: " + port + " http://localhost:3000/");
   // console.log("Server Started successfully!!\nListening to port: http://localhost:3000/")
 });
-
